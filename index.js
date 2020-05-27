1abbb6783a6bdef5539adbf5e535ab9803e4a541
Jamie Berrier <berrier.jamie@gmail.com>
2020-05-27 18:52:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,6 @@ function writeCards(names, event_name) {
 }
 
 // write a function, countDown, that takes in any positive integer and, starting from that number, counts down to zero using console.log(). 
+function countDown() {
+  
+}
