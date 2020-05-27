966a9a408c575d7bb8abf79994e540982c5d5d77
Jamie Berrier <berrier.jamie@gmail.com>
2020-05-27 18:44:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solutions in this file
 // build a function named writeCards() that accepts two arguments: an array of string names, and an event name.
 function writeCards(names, event_name) {
+  cards = []
   // Create a for loop with a counter that starts at 0 and increments at the end of each loop.
   // The condition should halt the for loop after the last name in the array is printed out in the loop body.
   for (let i = 0; i < names.length; i++) {
